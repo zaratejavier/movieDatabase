e7dc4d3f91f599b80dfe1e6cae56978280d2c6ec
Javier Zarate <zarate.javier.jz@gmail.com>
2020-06-29 21:27:35 -0600

    first commit

--- client/src/components/FetchUser.js
+++ client/src/components/FetchUser.js
@@ -3,15 +3,18 @@ import { AuthContext } from "../providers/AuthProvider"
 import axios from "axios"
 
 export default function FetchUser(props){
-  const [loaded, setloaded] = useState(false)
+  const [loaded, setLoaded] = useState(false)
   const {authenticated, setUser} = useContext(AuthContext)
 
-  useEffect(async () =>{
-    if(!authenticated){
-      await checkLocalToken();
+  useEffect(() => {
+    async function checkUser() { // can be async
+      if (!authenticated) {
+        await checkLocalToken();
+      }
+      setLoaded(true);
     }
-    setloaded(true) //set loaded to true and render all of the other components
-  },[])
+    checkUser();
+  }, []);
 
 
   // The checkLocalToken function will check to see if we have a token saved in localStorage and retrieve it if it finds one.  
